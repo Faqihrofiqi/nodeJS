a53f7b02eb8d717a30e57a4b4e5d7393107e0f93
Faqihrofiqi <21sa1076@mhs.amikompurwokerto.ac.id>
2023-09-30 03:30:18 +0700

    feat(js):create module calculator and file soal1.js

---
+++ rumus.js
@@ -0,0 +1,7 @@
+exports.luas = function(x,y){
+    return x*y;
+};
+
+exports.keliling = function(x,y){
+    return 2*(x+y);
+};
---
+++ soal1.js
@@ -0,0 +1,5 @@
+var calculator=require('./rumus');
+var x=50, y=8;
+console.log("luas dari persegi panjang dengan panjang %s dan lebar %s adalah "+calculator.luas(x,y), x,y);
+
+console.log("keliling dari persegi panjang dengan panjang %s dan lebar %s adalah "+calculator.keliling(x,y), x,y);
